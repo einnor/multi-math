c1d23527569f3caa4d4615eba0dcde03f19903f4
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-10-01 18:36:40 +0300

    Taking advantage of built-in types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-const startGame = () => {
+const startGame = (): void => {
   // starting a new game
-  const messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
-}
+  const playerName: string = 'Audrey';
+  logPlayer(playerName);
+  const messagesElement: HTMLElement | null = document.getElementById('messages');
+  if (messagesElement) {
+    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+  }
+};
+
+const logPlayer = (name: string): void => {
+  console.log(`New game starting for player: ${name}`);
+};
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
